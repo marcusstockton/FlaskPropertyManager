3912e270cee672b5dee7629bacbd621dca515081
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2020-05-11 20:33:32 +0100

    initial commit

---
+++ app/main/util/dto.py
@@ -0,0 +1,22 @@
+from flask_restx import Namespace, fields
+
+
+class UserDto:
+    api = Namespace('user', description='user related operations')
+    user = api.model('user', {
+        'email': fields.String(required=True, description='user email address'),
+        'username': fields.String(required=True, description='user username'),
+        'password': fields.String(required=True, description='user password'),
+		'firstname': fields.String(required=True, description='user first name'),
+		'lastname': fields.String(required=True, description='user last name'),
+		'date_of_birth':fields.Date(required=False, description='user date of birth'),
+        'public_id': fields.String(description='user Identifier')
+    })
+	
+	
+class AuthDto:
+    api = Namespace('auth', description='authentication related operations')
+    user_auth = api.model('auth_details', {
+        'email': fields.String(required=True, description='The email address'),
+        'password': fields.String(required=True, description='The user password '),
+    })
