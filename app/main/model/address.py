cab04387a6d99f2a24f9cd95e27667d21593f642
Marcus Stockton <marcus.stockton@8technology.com>
2020-06-01 13:30:16 +0100

    Added Portfolio and Property models

---
+++ app/main/model/address.py
@@ -0,0 +1,18 @@
+from .. import db
+from .property import Property
+
+class Address(db.Model):
+	""" Address Model for storing addresses """
+	__tablename__ = "address"
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	line_1 = db.Column(db.String(100))
+	line_2 = db.Column(db.String(100))
+	line_3 = db.Column(db.String(100), nullable=True)
+	post_code = db.Column(db.String(100))
+	town = db.Column(db.String(100), nullable=True)
+	city = db.Column(db.String(100), nullable=True)
+	property_id = db.Column(db.Integer, db.ForeignKey(Property.id))
+	property = db.relationship(Property, uselist=False, back_populates="address")
+	
+	def __repr__(self):
+		return "<Address '{} {} {} {} {} {}'>".format(self.line_1, self.line_2, self.line_3, self.post_code, self.town, self.city)
---
+++ app/main/model/property.py
@@ -0,0 +1,15 @@
+from .. import db
+from .portfolio import Portfolio
+
+
+class Property(db.Model):
+	""" Property Model for storing properties """
+	__tablename__ = "property"
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	portfolio_id = db.Column(db.Integer, db.ForeignKey(Portfolio.id))
+	address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
+	address = db.relationship("address", back_populates="property")
+	purchase_price = db.Column(db.Float(precision='10, 2'), nullable=True)
+	purchase_date = db.Column(db.DateTime, nullable=True)
+	monthly_rental_price = db.Column(db.Float(precision='10, 2'), nullable=True)
+
