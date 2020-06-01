668143d8b9821e066889aff8e5f24f64ff53ad6f
Marcus Stockton <marcus.stockton@8technology.com>
2020-06-01 10:44:16 +0100

    initial commit

--- app/main/util/decorator.py
+++ app/main/util/decorator.py
@@ -7,7 +7,6 @@ from app.main.service.auth_helper import Auth
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-
         data, status = Auth.get_logged_in_user(request)
         token = data.get('data')
 
@@ -22,7 +21,6 @@ def token_required(f):
 def admin_token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-
         data, status = Auth.get_logged_in_user(request)
         token = data.get('data')
 
