fdc1bdae123fd3ef6e45615aac03b9ed0a169323
Marcus Stockton <marcus.stockton@8technology.com>
2020-06-01 13:30:16 +0100

    Added Portfolio and Property models

---
+++ app/main/model/portfolio.py
@@ -0,0 +1,16 @@
+from .. import db
+import datetime
+from .user import User
+
+
+class Portfolio(db.Model):
+	""" Portfolio Model for storing portfolio's """
+	__tablename__ = "portfolio"
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(100))
+	created_on = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+	owner = db.Column(db.Integer, db.ForeignKey(User.id, ondelete='CASCADE'))
+	properties = db.relationship("Property")
+
+	def __repr__(self):
+		return "<Portfolio '{}'>".format(self.name)
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from flask_script import Manager
 from app.main import create_app, db
 from app import blueprint
 
-from app.main.model import user, blacklist
+from app.main.model import user, blacklist, portfolio, property, address
 
 app = create_app(os.getenv('PROPERTYMANAGER_ENV') or 'dev')
 app.register_blueprint(blueprint)
