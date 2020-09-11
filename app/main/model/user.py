d5d2f2014c2f2132445a45e7053052f525459bec
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2020-09-11 22:53:20 +0100

    Added database seeder.py Called via python manage.py seed

--- app/main/model/user.py
+++ app/main/model/user.py
@@ -6,83 +6,84 @@ from ..config import key
 
 
 class User(db.Model):
-	""" User Model for storing user related details """
-	__tablename__ = "user"
+    """ User Model for storing user related details """
+    __tablename__ = "user"
 
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	email = db.Column(db.String(255), unique=True, nullable=False)
-	registered_on = db.Column(db.DateTime, nullable=False)
-	admin = db.Column(db.Boolean, nullable=False, default=False)
-	public_id = db.Column(db.String(100), unique=True)
-	username = db.Column(db.String(50), unique=True)
-	password_hash = db.Column(db.String(100))
-	first_name = db.Column(db.String(100), nullable=True)
-	last_name = db.Column(db.String(100), nullable=True)
-	date_of_birth = db.Column(db.DateTime, nullable=True)
-	roles = db.relationship('Role', secondary='user_roles', backref=db.backref('user', lazy='dynamic'))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    public_id = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(50), unique=True)
+    password_hash = db.Column(db.String(100))
+    first_name = db.Column(db.String(100), nullable=True)
+    last_name = db.Column(db.String(100), nullable=True)
+    date_of_birth = db.Column(db.DateTime, nullable=True)
+    roles = db.relationship('Role', secondary='user_roles', backref=db.backref('user', lazy='dynamic'))
 
-	@property
-	def password(self):
-		raise AttributeError('password: write-only field')
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
 
-	@password.setter
-	def password(self, password):
-		self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+    @password.setter
+    def password(self, password):
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
 
-	def check_password(self, password):
-		return flask_bcrypt.check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
 
-	def encode_auth_token(self, user_id):
-		"""
+    def encode_auth_token(self, user_id):
+        """
 		Generates the Auth Token
 		:return: string
 		"""
-		try:
-			payload = {
-				'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
-				'iat': datetime.datetime.utcnow(),
-				'sub': user_id,
-				'username': User.query.filter_by(id=user_id).first().username
-			}
-			return jwt.encode(
-				payload,
-				key,
-				algorithm='HS256'
-			)
-		except Exception as e:
-			return e
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id,
+                'username': User.query.filter_by(id=user_id).first().username
+            }
+            return jwt.encode(
+                payload,
+                key,
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
 
-	@staticmethod  
-	def decode_auth_token(auth_token):
-		"""
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
 		Decodes the auth token
 		:param auth_token:
 		:return: integer|string
 		"""
-		try:
-			payload = jwt.decode(auth_token, key,algorithms=['HS256'])
-			is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
-			if is_blacklisted_token:
-				return 'Token blacklisted. Please log in again.'
-			else:
-				return payload['sub']
-		except jwt.ExpiredSignatureError:
-			return 'Signature expired. Please log in again.'
-		except jwt.InvalidTokenError:
-			return 'Invalid token. Please log in again.'
+        try:
+            payload = jwt.decode(auth_token, key, algorithms=['HS256'])
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
 
-	def __repr__(self):
-		return "<User '{}'>".format(self.username)
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
 
 
 class Role(db.Model):
-        __tablename__ = 'roles'
-        id = db.Column(db.Integer(), primary_key=True)
-        name = db.Column(db.String(50), unique=True)
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
 
 # Define the UserRoles association table
 class UserRoles(db.Model):
-	__tablename__ = 'user_roles'
-	id = db.Column(db.Integer(), primary_key=True)
-	user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
-	role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
