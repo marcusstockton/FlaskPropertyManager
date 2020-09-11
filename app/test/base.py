26f4e31d653bcfe1ceb91dee1360a39c3235c1b9
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2020-09-11 18:38:05 +0100

    Added user_service unit test

--- app/test/base.py
+++ app/test/base.py
@@ -1,5 +1,6 @@
 from flask_testing import TestCase
 from app.main import db
+from app.main.model.user import Role
 from manage import app
 
 
@@ -11,6 +12,11 @@ class BaseTestCase(TestCase):
 
     def setUp(self):
         db.create_all()
+        if Role.query.first() is None:
+            owner_role = Role(name="Owner")
+            admin_role = Role(name="Admin")
+            db.session.add(owner_role)
+            db.session.add(admin_role)
         db.session.commit()
 
     def tearDown(self):
--- app/test/test_user_model.py
+++ app/test/test_user_model.py
@@ -31,6 +31,5 @@ class TestUserModel(BaseTestCase):
         self.assertTrue(isinstance(auth_token, bytes))
         self.assertTrue(User.decode_auth_token(auth_token.decode("utf-8") ) == 1)
 
-
 if __name__ == '__main__':
     unittest.main()
