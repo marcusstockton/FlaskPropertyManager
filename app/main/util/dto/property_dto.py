d3fa9885c16ab8edef3d04b60d1411f9af5349f9
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2023-07-22 17:40:58 +0100

    starting to lint files with black Fixing some unit tests. More need fixing

--- app/main/util/dto/property_dto.py
+++ app/main/util/dto/property_dto.py
@@ -14,43 +14,81 @@ class Base64Decoder(fields.Raw):
 
 
 class PropertyDto:
-    api = Namespace('Property', description='Property related operations')
+    api = Namespace("Property", description="Property related operations")
 
-    property_pictures = api.model('PropertyImages', {
-        'id': fields.String(required=True, description='id'),
-        'created_date': fields.DateTime(required=False, description='date created'),
-        'updated_date': fields.DateTime(required=False, description='date last updated'),
-        'file_name': fields.String(required=True, description='file name of image'),
-        'image_base_64': Base64Decoder(attribute="image"),
-    })
+    property_pictures = api.model(
+        "PropertyImages",
+        {
+            "id": fields.String(required=True, description="id"),
+            "created_date": fields.DateTime(required=False, description="date created"),
+            "updated_date": fields.DateTime(
+                required=False, description="date last updated"
+            ),
+            "file_name": fields.String(required=True, description="file name of image"),
+            "image_base_64": Base64Decoder(attribute="image"),
+        },
+    )
 
-    property = api.model('Property', {
-        'id': fields.String(required=True, description='id'),
-        'portfolio_id': fields.String(required=True, description='portfolio id'),
-        'purchase_price': fields.Float(required=True, description='purchase price'),
-        'purchase_date': fields.DateTime(required=True, description='purchase date'),
-        'monthly_rental_price': fields.Float(required=True, description='monthly rental price'),
-        'address': fields.Nested(AddressDto.address, required=True, description='address'),
-        'tenants': fields.List(fields.Nested(TenantDto.tenant), required=False, description='tenants'),
-        'created_date': fields.DateTime(required=False, description='date created'),
-        'updated_date': fields.DateTime(required=False, description='date last updated'),
-        'property_pics': fields.List(fields.Nested(property_pictures), required=False, description='images')
-    })
+    property = api.model(
+        "Property",
+        {
+            "id": fields.String(required=True, description="id"),
+            "portfolio_id": fields.String(required=True, description="portfolio id"),
+            "purchase_price": fields.Float(required=True, description="purchase price"),
+            "purchase_date": fields.DateTime(
+                required=True, description="purchase date"
+            ),
+            "monthly_rental_price": fields.Float(
+                required=True, description="monthly rental price"
+            ),
+            "address": fields.Nested(
+                AddressDto.address, required=True, description="address"
+            ),
+            "tenants": fields.List(
+                fields.Nested(TenantDto.tenant), required=False, description="tenants"
+            ),
+            "created_date": fields.DateTime(required=False, description="date created"),
+            "updated_date": fields.DateTime(
+                required=False, description="date last updated"
+            ),
+            "property_pics": fields.List(
+                fields.Nested(property_pictures), required=False, description="images"
+            ),
+        },
+    )
 
-    property_list = api.model('Property', {
-        'id': fields.String(required=True, description='id'),
-        'portfolio_id': fields.String(required=True, description='portfolio id'),
-        'purchase_price': fields.Float(required=True, description='purchase price'),
-        'purchase_date': fields.DateTime(required=True, description='purchase date'),
-        'monthly_rental_price': fields.Float(required=True, description='monthly rental price'),
-        'address': fields.Nested(AddressDto.address, required=True, description='address'),
-        'created_date': fields.DateTime(required=False, description='date created'),
-        'updated_date': fields.DateTime(required=False, description='date last updated'),
-    })
+    property_list = api.model(
+        "Property",
+        {
+            "id": fields.String(required=True, description="id"),
+            "portfolio_id": fields.String(required=True, description="portfolio id"),
+            "purchase_price": fields.Float(required=True, description="purchase price"),
+            "purchase_date": fields.DateTime(
+                required=True, description="purchase date"
+            ),
+            "monthly_rental_price": fields.Float(
+                required=True, description="monthly rental price"
+            ),
+            "address": fields.Nested(
+                AddressDto.address, required=True, description="address"
+            ),
+            "created_date": fields.DateTime(required=False, description="date created"),
+            "updated_date": fields.DateTime(
+                required=False, description="date last updated"
+            ),
+        },
+    )
 
-    property_create = api.model('Property', {
-        'purchase_price': fields.Float(required=True, description='purchase price'),
-        'purchase_date': fields.Date(required=True, description='purchase date'),
-        'monthly_rental_price': fields.Float(required=True, description='monthly rental price'),
-        'address': fields.Nested(AddressDto.address_create, required=True, description='address'),
-    })
+    property_create = api.model(
+        "Property",
+        {
+            "purchase_price": fields.Float(required=True, description="purchase price"),
+            "purchase_date": fields.Date(required=True, description="purchase date"),
+            "monthly_rental_price": fields.Float(
+                required=True, description="monthly rental price"
+            ),
+            "address": fields.Nested(
+                AddressDto.address_create, required=True, description="address"
+            ),
+        },
+    )
