f9d579c19d11da60e0da89dc94fea3fa256d6fb5
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2025-07-15 20:22:21 +0100

    Update formatter in VSCode settings and improve error logging in base test case

--- app/test/base.py
+++ app/test/base.py
@@ -4,9 +4,10 @@ from flask_testing import TestCase
 from sqlalchemy.exc import IntegrityError
 
 from app.main import create_app, db
+from app.main.model.blacklist import BlacklistToken
 # Import all models that need tables before calling db.create_all().
 # This will ensure all tables, including blacklist_tokens, are created for your tests.
-from app.main.model import (user, portfolio, property as property_model, 
+from app.main.model import (user, portfolio, property as property_model,
                             address, tenant, blacklist)
 from manage import app
 
@@ -66,4 +67,5 @@ class BaseTestCase(TestCase):
             db.session.commit()
         except IntegrityError as e:
             db.session.rollback()
-            app.logger.error("Failed seeding test database: %s", e, exc_info=True)
+            app.logger.error(
+                "Failed seeding test database: %s", e, exc_info=True)
